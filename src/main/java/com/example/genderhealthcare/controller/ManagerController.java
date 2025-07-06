d259797b83712a7e0916fa800dca91cbb8d78f48
MinhTri070205 <trinm3468@ut.edu.vn>
2025-07-06 19:35:34 +0700

    Add files via upload

---
+++ src/main/java/com/example/genderhealthcare/controller/ManagerController.java
@@ -0,0 +1,15 @@
+package com.example.genderhealthcare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/manager")
+public class ManagerController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "manager/home";
+    }
+}
