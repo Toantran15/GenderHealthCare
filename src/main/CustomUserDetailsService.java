e2a6dcb1a4bae65583c99f8857ee908712e7a12e
MinhTri070205 <trinm3468@ut.edu.vn>
2025-07-06 19:27:10 +0700

    Add files via upload

---
+++ src/main/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.genderhealthcare.service;
+
+import com.example.genderhealthcare.entity.User;
+import com.example.genderhealthcare.repository.UserRepository;
+import com.example.genderhealthcare.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new CustomUserDetails(user);
+    }
+
+
+}
